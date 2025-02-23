3b69938a90260f92308af97c201b5ad396eb1666
sen-tou <29461308+sen-tou@users.noreply.github.com>
2025-02-23 11:58:28 +0100

    --wip-- [skip ci]

--- circular.h
+++ circular.h
@@ -1,5 +1,6 @@
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 20
@@ -7,17 +8,18 @@
 
 typedef struct
 {
-    size_t *head;
-    size_t *tail;
-    // instead of fixed buffer size malloc could be used to make it more
-    // dynamic, for now a buffer on the stack is fine
-    uint8_t buffer[BUFFER_SIZE];
-    bool full;
+  uint8_t *head;
+  uint8_t *tail;
+  // instead of fixed buffer size malloc could be used to make it more
+  // dynamic, for now a buffer on the stack is fine
+  uint8_t buffer[BUFFER_SIZE];
+  bool full;
 } Ring_buffer;
-typedef Ring_buffer *R_ptr;
 
-R_ptr Ring_buffer_init();
+Ring_buffer Ring_buffer_init();
 
-void Ring_buffer_reset(R_ptr buf);
+void Ring_buffer_reset(Ring_buffer buf);
 
-bool Ring_buffer_add(R_ptr buf, uint8_t data);
+bool Ring_buffer_add(Ring_buffer buf, uint8_t data);
+
+bool Ring_buffer_is_full(Ring_buffer buf);
--- main.c
+++ main.c
@@ -4,5 +4,9 @@
 
 int main()
 {
+    Ring_buffer buf = Ring_buffer_init();
+
+    Ring_buffer_add(buf, 8);
+
     return 1;
 }
